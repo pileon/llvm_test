948e4675ee1ad2bbefa54af75ad8ed12c70f5868
Joachim Pileborg <arrow@pileborg.se>
2018-04-11 21:13:46 +0200

    Source file templates

---
+++ .idea/fileTemplates/internal/C Header File.h
@@ -0,0 +1,6 @@
+#parse("C File Header.h")
+#[[#pragma once]]#
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+#[[#endif]]# //${INCLUDE_GUARD}
---
+++ .idea/fileTemplates/internal/C++ Class Header.h
@@ -0,0 +1,14 @@
+#parse("C File Header.h")
+#[[#pragma once]]#
+#[[#ifndef]]# ${INCLUDE_GUARD}
+#[[#define]]# ${INCLUDE_GUARD}
+
+${NAMESPACES_OPEN}
+
+class ${NAME} {
+
+};
+
+${NAMESPACES_CLOSE}
+
+#[[#endif]]# //${INCLUDE_GUARD}
